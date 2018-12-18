66e4f31086ab8bcead9a005602979b6d42ab5b93
RICHARD-PC\RICHARD <ta3agustinruizrojo@gmail.com>
2018-12-18 11:31:23 +0100

    002

---
+++ IA/Assets/Scripts/IA_Skeleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Skeleton : MonoBehaviour {
+
+	public enum States {Idle, Perseguir, Atacar, Volver};
+	public States miestado = States.Idle;
+	public GameObject Player;
+
+	public Transform target;
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
