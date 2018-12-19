833238a84295fa0c012afb285555f8e4229d725a
RICHARD-PC\RICHARD <ta3agustinruizrojo@gmail.com>
2018-12-19 22:35:50 +0100

    Definitivo

---
+++ IA/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    //private Animator anim;
+    private Rigidbody myrigi;
+
+    // Use this for initialization
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+        myrigi = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //To move in vertically.
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * 5, Space.World);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 5, Space.World);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        }
+
+        //To move in horizontally.
+        if (Input.GetAxis("Horizontal") > 0)
+
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 5, Space.World);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 5, Space.World);
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        }
+
+    }
+}
---
+++ IA/Assets/Scripts/IA.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA : MonoBehaviour
+{
+
+    public enum States { Idle, Walk, Attack };
+    public States miestado = States.Idle;
+    public GameObject Player;
+    private Animator anim;
+    private Rigidbody myrigi;
+
+    public Transform target;
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        myrigi = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (miestado)
+        {
+            case States.Idle:
+                Idle();
+                break;
+            case States.Walk:
+                Walk();
+                break;
+            case States.Attack:
+                Attack();
+                break;
+            default:
+                print("Incorrect State");
+                break;
+        }
+    }
+
+    void Idle()
+
+    {
+        anim.Play("Idle");
+
+        if (Vector3.Distance(transform.position, Player.transform.position) > 1 && Vector3.Distance(transform.position, Player.transform.position) < 8)
+        {
+            miestado = States.Walk;
+        }
+
+    }
+
+
+    void Walk()
+    {
+        anim.Play("Walk");
+
+        if (Vector3.Distance(transform.position, Player.transform.position) > 8)
+        {
+            miestado = States.Idle;
+        }
+
+        if (Vector3.Distance(transform.position, Player.transform.position) < 2)
+        {
+            miestado = States.Attack;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+
+
+    }
+
+    void Attack()
+    {
+        anim.Play("Attack");
+
+        if (Vector3.Distance(transform.position, Player.transform.position) > 2)
+        {
+            miestado = States.Walk;
+        }
+
+        if (Vector3.Distance(transform.position, Player.transform.position) > 8)
+        {
+            miestado = States.Idle;
+        }
+
+
+
+    }
+}
